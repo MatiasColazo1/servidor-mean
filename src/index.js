fda02c484a7e0ba5c5e10bd4772df425f38c56d3
MatiasColazo1 <maty_colazo@hotmail.com>
2023-10-27 16:46:30 -0300

    actualizacion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 
+//base de datos
+require('./database');
 
+//json
+app.use(express.json());
+
+//rutas
+app.use('/api', require('./routes/index'))
 
 app.listen(3000);
 console.log('Servidor en el puerto: ', 3000)
---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+const { Router } = require('express');
+const router = Router();
+
+const User = require('../models/User');
+
+const jwt = require('jsonwebtoken');
+
+router.get('/', (req, res) => res.send('Hola mundo'));
+
+router.post('/signup', async (req, res) => {
+    const { email, password } = req.body;
+    const newUser = new User({email, password});
+    await newUser.save();
+    
+    const token = jwt.sign({ _id: newUser._id }, 'secretKey')
+    res.status(200).json({token})
+
+})
+
+module.exports = router;
