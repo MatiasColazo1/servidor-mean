1729e9068bb387c104d5b198e041122c47a88e56
MatiasColazo1 <maty_colazo@hotmail.com>
2023-10-28 00:38:37 -0300

    verificacion de token

--- src/routes/index.js
+++ src/routes/index.js
@@ -49,7 +49,7 @@ router.get('/tasks', (req, res) => {
     ])
 })
 
-router.get('/private-tasks', (req, res) => {
+router.get('/private-tasks', verifyToken, (req, res) => {
     res.json([
         {
             _id: 1,
@@ -75,9 +75,19 @@ router.get('/private-tasks', (req, res) => {
 module.exports = router;
 
 //rutas privadas
+
 function verifyToken(req, res, next) {
-    if(!req.headers.authorization) {
+    if (!req.headers.authorization) {
         return res.status(401).send('No autorizado');
     }
-    
+
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token === 'null') {
+        return res.status(401).send('No autorizado');
+    }
+
+    const payload = jwt.verify(token, 'secretKey')
+    req.userUd = payload._id;
+    next();
 }
